a11698318b3f3a31c2b553e5f7ce94805e512060
Falchi <marcelonavarro11md@gmail.com>
2021-09-08 23:42:13 -0300

    Basic Static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,17 @@
+import DataTable from "components/DataTable";
+import Footer from "components/Footer";
+import NavBar from "components/NavBar";
+
 function App() {
   return (
-    <div>
-      <h1 className="text-primary">Olá Mundo</h1>
-    </div>
-    
+    <>
+      <NavBar />
+      <div className="container">
+         <h1 className="text-primary">Olá Mundo!</h1>
+      </div>
+      <DataTable />     
+      <Footer />
+    </>    
   );
 }
 
---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,12 @@
+function Footer() {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+                <div className="container">
+                    <p className="text-light">App desenvolvido por <a href="https://www.linkedin.com/in/marcelo-falchi-navarro-32a755216/" target="_blank" rel="noreferrer">Marcelo Falchi Navarro</a></p>
+                    <p className="text-light"><small><strong>Semana Spring React</strong><br />
+                        Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+                </div>
+            </footer>
+    );
+}
+export default Footer;
